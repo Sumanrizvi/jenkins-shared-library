5bacae494df05bd1704c89638e9d8ad0f6ae7f14
Suman Rizvi <sumanrizvi@gmail.com>
2023-11-28 10:32:38 -0800

    changed docker hub creds

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'GitHub-Creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t sumanrizvi/ec2-jenkins-pipeline:1.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push sumanrizvi/ec2-jenkins-pipeline:1.0'
